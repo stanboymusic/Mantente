045c6b3b682d9151f85b4b2f6c0057babbc4e23b
stanboymusic <adrcproducciones@gmail.com>
2025-10-19 07:41:07 -0400

    mi-carpeta/readme.md

---
+++ settings.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCyWx7aLAH8ETd8OB4rVJh0nF_q9JvaTEk",
+  authDomain: "mantente-5396f.firebaseapp.com",
+  projectId: "mantente-5396f",
+  storageBucket: "mantente-5396f.firebasestorage.app",
+  messagingSenderId: "440243140615",
+  appId: "1:440243140615:web:2504e71273b8b351acd2a2"
+};
